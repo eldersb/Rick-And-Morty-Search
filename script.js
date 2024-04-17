e6e4b1434ce852ca2e9387b291b1457146e75b63
eldersb <122701368+eldersb@users.noreply.github.com>
2024-04-17 18:55:40 -0300

    Implementando dropdown Selecionar persoangem

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+function SelPersonagem(){
+
+    
+
+    axios.get('https://rickandmortyapi.com/api/character')
+    .then(function(response){
+        
+        console.log(response.data.results);
+    })
+     .catch(function(error){
+         console.log(error);
+     })
+
+  
+    
+
+}
 
 
-axios.get('https://catfact.ninja/fact')
-.then(function(response){
-    console.log(response.data);
-})
-.catch(function(error){
-    console.log(error);
-})
+var app = {
+    characters: [],
+    isOpen: false
+};
+
+
+
+
+
+
+
