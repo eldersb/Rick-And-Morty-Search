ec48f2e6ea1c8932ff2104b38b908e212cd32002
eldersb <122701368+eldersb@users.noreply.github.com>
2024-04-18 09:59:22 -0300

    Commit

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-function SelPersonagem(){
 
-    
 
-    axios.get('https://rickandmortyapi.com/api/character')
-    .then(function(response){
+
+const app = () => {
+    return {
+        characters: [],
+        Isopen: true,
         
-        console.log(response.data.results);
-    })
-     .catch(function(error){
-         console.log(error);
-     })
-
-  
+    init(){
+        axios.get('https://rickandmortyapi.com/api/character')
+        .then((response) => {
+            this.characters = response.data.results;
+            console.log(response.data.results);
+            console.log(this.characters)
+            console.log(this.Isopen)
+        })
+         .catch((error) =>{
+             console.log(error);
+         })
+    }
+    }
     
 
 }
 
 
-var app = {
-    characters: [],
-    isOpen: false
-};
+
+  
+    
+
+
+
 
 
 
